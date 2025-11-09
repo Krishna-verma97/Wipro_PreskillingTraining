172d45b788106c6424668433d9cd6ad1661439e9
Krishna-verma97 <kv945164@gmail.com>
2025-11-09 09:33:01 +0530

    Wipro_PreSkillingTaining_my_notes...

---
+++ Practise1.ps1
@@ -0,0 +1,102 @@
+﻿Write-Host "Hello,World!!!"
+<#
+Multiline comment
+#>
+write-host 'Krishna'
+clear
+
+#Conditionals
+
+$service = Get-Service -Name "Spooler"
+if($service.Status -eq "running"){
+write-host "Running."
+}
+else{
+write-host "spooler is stoped"
+}
+
+
+$Stopservice = "spooler","dnscache"
+foreach($serviceName in $Stopservice){
+write-host "Attempting to stop $serviceName..."
+stop-service -name $serviceName
+}
+
+$Stopservice = "notepad++","excel"
+foreach($serviceName in $Stopservice){
+write-host "Attempting to stop $serviceName..."
+stop-service -name $serviceName
+}
+
+$Stopservice = "spooler","dnscache"
+foreach($serviceName in $Stopservice){
+write-host "Attempting to stop $serviceName..."
+#stop-service -name $serviceName
+}
+
+
+
+function service{
+Get-Command -Name Get-Process
+}
+service
+
+
+function Get-SystemUptime {
+    (Get-Date) - (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+}
+
+# Now you can call the function like a regular cmdlet
+Get-SystemUptime
+
+
+write-host $pwd
+write-host $psversiontable
+
+Get-Process | Get-Member
+
+$processes = Get-Process
+$processes[1].Id
+
+$sample = "apple","mango"
+write-host $sample[-1]#last item
+
+
+$servers = "Server01", "Server02", "Server03"
+
+foreach ($server in $servers) {
+    Write-Host "Testing connection to $server..."
+    Test-Connection -ComputerName $server -Count 1
+}
+
+
+$names = "Krishna","Vishnu","Gauri"
+foreach($names in $names){
+write-host "$names is from the danger blood zone family dhasuuu"
+}
+
+
+
+Write-Host "This is Black text" -ForegroundColor Black
+Write-Host "This is DarkBlue text" -ForegroundColor DarkBlue
+Write-Host "This is DarkGreen text" -ForegroundColor DarkGreen
+Write-Host "This is DarkCyan text" -ForegroundColor DarkCyan
+Write-Host "This is DarkRed text" -ForegroundColor DarkRed
+Write-Host "This is DarkMagenta text" -ForegroundColor DarkMagenta
+Write-Host "This is DarkYellow text" -ForegroundColor DarkYellow
+Write-Host "This is Gray text" -ForegroundColor Gray
+Write-Host "This is DarkGray text" -ForegroundColor DarkGray
+Write-Host "This is Blue text" -ForegroundColor Blue
+Write-Host "This is Green text" -ForegroundColor Green
+Write-Host "This is Cyan text" -ForegroundColor Cyan
+Write-Host "This is Red text" -ForegroundColor Red
+Write-Host "This is Magenta text" -ForegroundColor Magenta
+Write-Host "This is Yellow text" -ForegroundColor Yellow
+Write-Host "This is White text" -ForegroundColor White
+
+
+
+ $services = "Spooler", "Dnscache", "TermService"
+
+# The pipeline automatically processes each item in the array
+$services | Get-Service
---
+++ Practise2.ps1
@@ -0,0 +1,56 @@
+﻿# In the Global Scope (after the script finishes)
+$global:MyGlobalVar = "This is a global variable"
+Write-Host "Global variable created in the script."
+
+# In the Script Scope
+$script:MyScriptVar = "This is a script variable"
+$MyNormalScriptVar = "This is a normal script variable"
+Write-Host "Script variables created."
+
+# A function that creates a new scope (Function Scope)
+function Test-FunctionScope {
+    # This variable is only visible inside this function
+    $MyFunctionVar = "This is a function variable"
+    Write-Host "Inside the function:"
+    Write-Host "  - MyGlobalVar is accessible: $MyGlobalVar"
+    Write-Host "  - MyScriptVar is accessible: $MyScriptVar"
+    Write-Host "  - MyNormalScriptVar is accessible: $MyNormalScriptVar"
+    Write-Host "  - MyFunctionVar is accessible: $MyFunctionVar"
+}
+
+# Run the function
+Test-FunctionScope
+
+# Back in the Script Scope
+Write-Host "---------------------------"
+Write-Host "Back in the script scope:"
+Write-Host "  - MyGlobalVar is accessible: $MyGlobalVar"
+Write-Host "  - MyScriptVar is accessible: $MyScriptVar"
+Write-Host "  - MyNormalScriptVar is accessible: $MyNormalScriptVar"
+# The next line will fail because the variable is out of scope!
+Write-Host "  - MyFunctionVar is accessible: $MyFunctionVar"
+
+Get-Service | Where-Object { $_.Status -eq 'Stopped' -or $_.Status -eq 'StopPending' }
+
+Get-Service | Where-Object { -not ($_.Status -eq 'Running')
+
+ # This command will generate an error because the service doesn't exist
+Get-Service -Name "NonExistentService" 2> C:\temp\errors.txt
+
+
+# Create a hashtable of parameters and their values
+$params = @{
+    Path = "C:\Windows\System32"
+    Force = $true
+    Recurse = $true
+}
+
+# Use the splatting operator (@) to pass the hashtable
+Get-ChildItem @params
+
+
+$services = Get-Service -name "spooler"
+if($services.status -eq "running"){
+write-host "Service exists."}
+else{
+write-host "Setvice doesn't exists."}
